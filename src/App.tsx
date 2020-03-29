d525185049b24e0cf008c983baaf687755345015
MatthieuLOBJOIS <lobjois.mt@gmail.com>
2020-03-29 20:27:46 +0200

    Creat components for home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import React, {FunctionComponent} from 'react';
+//Imports of dependencies
+import React, { FunctionComponent } from 'react';
+
+//Local imports
 import './App.css';
 
+//Components
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+
 const App: FunctionComponent = () => {
-	return <div className="App">hello</div>;
-}
+	return (
+		<div className="App">
+			<Header />
+			<Home />
+			<Footer />
+		</div>
+	);
+};
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from 'react';
+
+const Header: FunctionComponent = () => {
+	return <div>Header</div>;
+};
+
+export default Header;
