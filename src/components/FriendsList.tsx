2e52b5599a79534485a5a9781b13fd28a5bce232
MatthieuLOBJOIS <lobjois.mt@gmail.com>
2020-04-29 15:57:08 +0200

    Add icon messageChat and fix style list item

--- src/components/FriendsList.tsx
+++ src/components/FriendsList.tsx
@@ -1,53 +1,59 @@
 //Imports of dependencies
-import React from 'react';
-import { createStyles, makeStyles, Theme, withStyles } from '@material-ui/core/styles';
-import { List, ListItem, ListItemSecondaryAction, ListItemText, ListItemAvatar } from '@material-ui/core';
+import React, { useState } from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { List, ListItem, ListItemText, ListItemAvatar } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
+import ChatIcon from '@material-ui/icons/Chat';
 
 //Local imports*
 import Avatar from './Avatar';
 
-const useStyles = makeStyles((theme: Theme) =>
-	createStyles({
-		root: {
-			width: '100%',
-			maxWidth: 360,
-			backgroundColor: '#161c2e',
-			color: 'white'
-		},
-		close: {
-			color: '#161c2e'
-		}
-	})
-);
-
-const StyledListItem = withStyles((theme) => ({
-	root: {
-		'&:focus': {
-			backgroundColor: '#ef6c35',
-			'& .MuiListItemIcon-root, & .MuiListItemText-primary': {
-				color: '#161c2e'
-			}
-		}
-	}
-}))(ListItem);
-
 export default function FriendsList() {
+	const [ display, setDisplay ] = useState({ status: 'none', value: '' });
+	const [ listColor, setListColor ] = useState({ background: '#161c2e', color: 'white' });
+
+	const useStyles = makeStyles((theme: Theme) =>
+		createStyles({
+			root: {
+				width: '100%',
+				maxWidth: 360,
+				color: 'white'
+			},
+			listBackground: {
+				backgroundColor: listColor.background,
+				color: listColor.color
+			},
+			iconColor: {
+				color: '#161c2e',
+				display: display.status
+			}
+		})
+	);
+
 	const classes = useStyles();
+
+	const handleDisplayIconClick = (event: any) => {
+		//console.log(event.currentTarget.id);
+		setDisplay({ ...display, status: 'bloc', value: event.currentTarget.id });
+		setListColor({ ...listColor, background: '#ef6c35', color: '#161c2e' });
+	};
 
 	return (
 		<List dense className={classes.root}>
-			{[ 0, 1, 2, 3 ].map((value) => {
+			{[ 0, 1, 2, 3 ].map((value, index) => {
+				//console.log(value);
 				const labelId = `checkbox-list-secondary-label-${value}`;
 				return (
-					<StyledListItem key={value} button>
-						<ListItemAvatar>
-							<Avatar />
-						</ListItemAvatar>
-						<ListItemText id={labelId} primary={`Line item ${value + 1}`} />
-
-						<CloseIcon className={classes.close} />
-					</StyledListItem>
+					<div onClick={handleDisplayIconClick} id={`${value}`}>
+						<ListItem className={`${value}` === display.value ? classes.listBackground : ''} key={value}>
+							<ListItemAvatar>
+								<Avatar />
+							</ListItemAvatar>
+							<ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+							{`${value}` === display.value ? <ChatIcon className={classes.iconColor} /> : ''}
+							{`${value}` === display.value ? <CloseIcon className={classes.iconColor} /> : ''}
+						</ListItem>
+					</div>
 				);
 			})}
 		</List>
