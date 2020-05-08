5b33fbfdf1391b26bd4e1ddf2eb31d0441faa5b7
MatthieuLOBJOIS <lobjois.mt@gmail.com>
2020-05-08 23:06:53 +0200

    Add moment timestamp and update style message

--- src/components/ContainerMessages.tsx
+++ src/components/ContainerMessages.tsx
@@ -1,6 +1,7 @@
 //Imports of dependencies
 import React, { FunctionComponent, useEffect, useRef } from 'react';
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import moment from 'moment';
 
 type Props = {
 	talk: any;
@@ -9,7 +10,7 @@ type Props = {
 
 const useStyles = makeStyles((theme: Theme) => {
 	const colorMessage = 'white';
-	const paddingMessage = '1em';
+	const paddingMessage = '0 1em 1em';
 	const widthMessage = '50%';
 	const overflowWrapMessage = 'break-word';
 	return createStyles({
@@ -37,6 +38,18 @@ const useStyles = makeStyles((theme: Theme) => {
 			width: widthMessage,
 			overflowWrap: overflowWrapMessage,
 			alignSelf: 'flex-end'
+		},
+		timeUser: {
+			fontSize: '0.6em',
+			position: 'relative',
+			top: '-2.5em',
+			color: '#ef6c35'
+		},
+		timeFriends: {
+			fontSize: '0.6em',
+			position: 'relative',
+			top: '-2.5em',
+			color: '#161c2e'
 		}
 	});
 });
@@ -56,6 +69,9 @@ const ContainerMessages: FunctionComponent<Props> = ({ talk, myFriends }) => {
 						key={index}
 						className={myFriends.pseudo === message.pseudo ? classes.messageFriends : classes.messageUser}
 					>
+						<p className={myFriends.pseudo === message.pseudo ? classes.timeFriends : classes.timeUser}>
+							{moment(Number(message.timestamp)).format('lll')}
+						</p>
 						{message.message}
 					</p>
 				);
