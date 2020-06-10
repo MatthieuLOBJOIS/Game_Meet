4865c5adc02c5b6992df461009a515222b060820
MatthieuLOBJOIS <lobjois.mt@gmail.com>
2020-06-10 14:50:47 +0200

    Menu Responsive

--- src/components/Header/Menu/index.tsx
+++ src/components/Header/Menu/index.tsx
@@ -1,6 +1,6 @@
 //Imports of dependencies
 import React, { FunctionComponent } from 'react';
-import { Drawer, Button } from '@material-ui/core';
+import { Drawer, Button, Hidden } from '@material-ui/core';
 import ViewComfyIcon from '@material-ui/icons/ViewComfy';
 import Typography from '@material-ui/core/Typography';
 
@@ -37,37 +37,55 @@ const Menu: FunctionComponent<Props> = ({ logOut }) => {
 	};
 
 	const anchor = 'left';
+
+	const drawer = (
+		<div>
+			<Link to="/">
+				<img className={classes.logo} src={Logo} />
+			</Link>
+			<SearchBar />
+			<Typography className={classes.typo} variant="h6" component="h2">
+				Messages privés
+			</Typography>
+			<FriendsList />
+			<Button
+				onClick={() => {
+					logOut(sessionLogin.uid);
+					localStorage.removeItem('isUser');
+					localStorage.removeItem('isLogged');
+				}}
+				color="secondary"
+			>
+				Deconnexion
+			</Button>
+		</div>
+	);
+
 	return (
 		<div className={classes.menu}>
 			<React.Fragment key={anchor}>
 				<Button onClick={toggleDrawer(anchor, true)}>
 					<ViewComfyIcon fontSize="large" color="primary" classes={{ colorPrimary: classes.button }} />
 				</Button>
-				<Drawer
-					classes={{ paperAnchorLeft: classes.paperAnchorLeft }}
-					anchor={anchor}
-					open={state[anchor]}
-					onClose={toggleDrawer(anchor, false)}
-				>
-					<Link to="/">
-						<img className={classes.logo} src={Logo} />
-					</Link>
-					<SearchBar />
-					<Typography className={classes.typo} variant="h6" component="h2">
-						Messages privés
-					</Typography>
-					<FriendsList />
-					<Button
-						onClick={() => {
-							logOut(sessionLogin.uid);
-							localStorage.removeItem('isUser');
-							localStorage.removeItem('isLogged');
+				<Hidden smUp implementation="css">
+					<Drawer
+						classes={{ paperAnchorLeft: classes.paperAnchorLeft }}
+						anchor={anchor}
+						open={state[anchor]}
+						variant="temporary"
+						ModalProps={{
+							keepMounted: true // Better open performance on mobile.
 						}}
-						color="secondary"
+						onClose={toggleDrawer(anchor, false)}
 					>
-						Deconnexion
-					</Button>
-				</Drawer>
+						{drawer}
+					</Drawer>
+				</Hidden>
+				<Hidden xsDown implementation="css">
+					<Drawer classes={{ paperAnchorLeft: classes.paperAnchorLeft }} variant="permanent" open>
+						{drawer}
+					</Drawer>
+				</Hidden>
 			</React.Fragment>
 		</div>
 	);
